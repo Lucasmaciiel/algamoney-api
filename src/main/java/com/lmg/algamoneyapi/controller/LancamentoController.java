32ff0942be45567dbb0e28c1aefa0fd11407fa05
Lucasmaciiel <lucasmacielgois@gmail.com>
2021-01-24 21:15:03 -0300

    Validando Inconsistencia DataIntegrityViolationException

--- src/main/java/com/lmg/algamoneyapi/controller/LancamentoController.java
+++ src/main/java/com/lmg/algamoneyapi/controller/LancamentoController.java
@@ -2,14 +2,17 @@ package com.lmg.algamoneyapi.controller;
 
 import com.lmg.algamoneyapi.model.Categoria;
 import com.lmg.algamoneyapi.model.Lancamento;
+import com.lmg.algamoneyapi.model.Pessoa;
 import com.lmg.algamoneyapi.repository.LancamentoRepository;
 import lombok.Builder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,6 +21,17 @@ import java.util.List;
 public class LancamentoController {
 
     private final LancamentoRepository repository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Lancamento> save(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+        Lancamento obj = repository.save(lancamento);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(obj.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+        return ResponseEntity.created(uri).body(obj);
+    }
 
     @GetMapping("/{codigo}")
     public ResponseEntity<Lancamento> findById(@PathVariable(name = "codigo") Long id) {
