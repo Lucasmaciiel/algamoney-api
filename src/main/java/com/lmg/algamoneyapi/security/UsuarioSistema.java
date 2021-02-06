7be237e25f1c32bcc2874a6fccd7f87e5182565c
Lucasmaciiel <lucasmacielgois@gmail.com>
2021-02-06 14:40:03 -0300

    Adiciona Nome do usuário no token JWT

---
+++ src/main/java/com/lmg/algamoneyapi/security/UsuarioSistema.java
@@ -0,0 +1,20 @@
+package com.lmg.algamoneyapi.security;
+
+import com.lmg.algamoneyapi.model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class UsuarioSistema extends User {
+
+    private Usuario usuario;
+
+    public UsuarioSistema(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+        super(usuario.getEmail(), usuario.getSenha(), authorities);
+        this.usuario = usuario;
+    }
+    public Usuario getUsuario(){
+        return usuario;
+    }
+}
